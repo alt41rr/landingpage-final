b92fb115252d22bc23fc4b3fee064ed7e05b6cc7
Kjoell29 <kevinjoelclief57@gmail.com>
2023-06-27 08:40:17 +0800

    membuat about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,24 +1,24 @@
-import React from 'react'
-import about from '../../assets/img/abaout.png';
+import React from "react";
+import about from "../../assets/img/abaout.png";
 
 const index = () => {
-    return (
-        <section id="about">
-            <div className="about-wrapper container">
-                <div className="about-text">
-                <p className="small">About Us</p>
-                <h2>ABONKU</h2>
-                <p>
-                    Abonku merupakan produk yang disediakan untuk anda yang sedang mencari oleh-oleh atau sekedar ingin makan makanan siap santap. Kami hadir dengan VISI MISI yang jelas yaitu Menjadikan Abonku sebagai oleh-oleh khas utama dari kota
-                    Bitung di tahun 2022, dengan memproduksi makanan yang murah, higenis, bermutu tinggi serta mudah di jangkau
-                </p>
-                </div>
-                <div className="about-img">
-                <img src={about} alt="food" />
-                </div>
-            </div>
-        </section>
-    )
-}
+  return (
+    <section id="about">
+      <div className="about-wrapper container">
+        <div className="about-text">
+          <p className="small">About Us</p>
+          <h2>ABONKU</h2>
+          <p>
+            Abonku merupakan produk yang disediakan untuk anda yang sedang mencari oleh-oleh atau sekedar ingin makan makanan siap santap. Kami hadir dengan VISI MISI yang jelas yaitu Menjadikan Abonku sebagai oleh-oleh khas utama dari kota
+            Bitung di tahun 2022, dengan memproduksi makanan yang murah, higenis, bermutu tinggi serta mudah di jangkau
+          </p>
+        </div>
+        <div className="about-img">
+          <img src={about} alt="food" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default index
+export default index;
