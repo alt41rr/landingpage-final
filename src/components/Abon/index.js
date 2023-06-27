3dabc72a5abff5e0efb5842fb4e5568e36882afb
Rionaldo16 <rionaldosiaman@gmail.com>
2023-06-27 08:44:14 +0800

    menambahkan section abon

--- src/components/Abon/index.js
+++ src/components/Abon/index.js
@@ -1,33 +1,43 @@
-import React from 'react'
+import React from "react";
 
 const index = () => {
-    return (
-        <div>
-            <section id="food">
-            <h2>Types Abon</h2>
-            <div class="food-container container">
-            <div class="food-type fruite">
-                <div class="img-container">
-                <img src="http://api.omiyago.com/webroot/img/p/1/5/4/5/1545.jpg" alt="error" />
-                <div class="img-content">
-                    <h3>Tuna</h3>
-                    <a href="#food-menu" class="btn btn-primary" target="blank">Order now</a>
-                </div>
-                </div>
+  return (
+    <div>
+      <section id="food">
+        <h2>Types Abon</h2>
+        <div class="food-container container">
+          <div class="food-type fruite">
+            <div class="img-container">
+              <img
+                src="http://api.omiyago.com/webroot/img/p/1/5/4/5/1545.jpg"
+                alt="error"
+              />
+              <div class="img-content">
+                <h3>Tuna</h3>
+                <a href="#food-menu" class="btn btn-primary" target="blank">
+                  Order now
+                </a>
+              </div>
             </div>
-            <div class="food-type vegetable">
-                <div class="img-container">
-                <img src="https://asset.kompas.com/crops/BZn6887HCTVj6Ks-GcqV4Qdf6q8=/0x0:698x465/750x500/data/photo/2021/02/25/60373577eac89.jpg" alt="error" />
-                <div class="img-content">
-                    <h3>Cakalang</h3>
-                    <a href="#food-menu" class="btn btn-primary" target="blank">Order now</a>
-                </div>
-                </div>
+          </div>
+          <div class="food-type vegetable">
+            <div class="img-container">
+              <img
+                src="https://asset.kompas.com/crops/BZn6887HCTVj6Ks-GcqV4Qdf6q8=/0x0:698x465/750x500/data/photo/2021/02/25/60373577eac89.jpg"
+                alt="error"
+              />
+              <div class="img-content">
+                <h3>Cakalang</h3>
+                <a href="#food-menu" class="btn btn-primary" target="blank">
+                  Order now
+                </a>
+              </div>
             </div>
-            </div>
-            </section>
+          </div>
         </div>
-    )
-}
+      </section>
+    </div>
+  );
+};
 
-export default index
+export default index;
